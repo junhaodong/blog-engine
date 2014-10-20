9b9a2f6e7dd2a00fbd5f34b9be1855b26fc491d7
Jenny Baran <jenny.baran@gmail.com>
2014-10-20 12:47:18 -0400

    app.py & some template stuff

---
+++ app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return render_template("home.html")
+
+@app.route("/<name>") #SEE FLASK DOCUMENTATION
+def post():
+    return render_template("post.html")
+
+if __name__ == "__main__":
+    app.debug=True
+    app.run()
